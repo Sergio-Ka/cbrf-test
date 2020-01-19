d07773bc860c9e5e40be46afff132fb60a8899d9
Sergio84 <kin-gall@mail.ru>
2020-01-20 03:29:04 +0700

    Updates

---
+++ precache-manifest.373af4dccc1409e665216357cbc818e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d45c6134c6a4c6adc980b537f24c54",
+    "url": "/cbrf-test/index.html"
+  },
+  {
+    "revision": "41db7a0251a22389bef8",
+    "url": "/cbrf-test/static/css/main.29fde003.chunk.css"
+  },
+  {
+    "revision": "a10e7ce3260f9cc5134c",
+    "url": "/cbrf-test/static/js/2.938ec3c1.chunk.js"
+  },
+  {
+    "revision": "a98948846f5725fb001ee7ad39723123",
+    "url": "/cbrf-test/static/js/2.938ec3c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41db7a0251a22389bef8",
+    "url": "/cbrf-test/static/js/main.eb1bd7a6.chunk.js"
+  },
+  {
+    "revision": "1f7b53e5e7d13fcce50a",
+    "url": "/cbrf-test/static/js/runtime-main.53d18667.js"
+  }
+]);
